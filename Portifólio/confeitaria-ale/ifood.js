ab5b538456e712c4dfd6f5527d7973c962f41fc2
alexdias777 <74875568+alexdias777@users.noreply.github.com>
2026-02-16 09:19:53 -0300

    Delete Portifólio directory
    excluir

--- Portifólio/confeitaria-ale/ifood.js
+++
@@ -1,4 +0,0 @@
-// Adicionar o evento de clique no botão
-    document.getElementById('GoIfood').addEventListener('click', function() {
-        window.open('https://www.ifood.com.br/delivery/alvorada-rs/bolos-doces-salada-de-frutas---bolo-de-pote-da-ale-passo-do-feijo/a61a583c-b947-4287-a6d0-8230133bbee1', '_blank');
-    });
