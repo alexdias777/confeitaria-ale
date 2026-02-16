083dd48eb2c9a5cc517224a7889ffbce16935466
alexdias777 <74875568+alexdias777@users.noreply.github.com>
2026-02-16 12:28:32 -0300

    Atualização do projeto confeitaria-ale

--- ifood.js
+++ ifood.js
@@ -1,9 +1,14 @@
+// Aguarda o carregamento completo do DOM
+document.addEventListener("DOMContentLoaded", function() {
+    // Evento para o botão "Finalizar Compra"
+    const finalizeButton = document.getElementById("GoIfood"); 
+    // Selecionando o botão no HTML
 
-// Adicionar o evento de clique no botão
-    document.getElementById('GoIfood').addEventListener('click', function() {
-        window.open('https://www.ifood.com.br/delivery/alvorada-rs/bolos-doces-salada-de-frutas---bolo-de-pote-da-ale-passo-do-feijo/a61a583c-b947-4287-a6d0-8230133bbee1', '_blank');
+    if (finalizeButton) {
+        finalizeButton.addEventListener('click', function() {
+            window.open('https://www.ifood.com.br/delivery/alvorada-rs/bolos-doces-salada-de-frutas---bolo-de-pote-da-ale-passo-do-feijo/a61a583c-b947-4287-a6d0-8230133bbee1', '_blank');
+        });
     }
-// Adicionar o evento de clique no botão
-    document.getElementById('GoIfood').addEventListener('click', function() {
-        window.open('https://www.ifood.com.br/delivery/alvorada-rs/bolos-doces-salada-de-frutas---bolo-de-pote-da-ale-passo-do-feijo/a61a583c-b947-4287-a6d0-8230133bbee1', '_blank');
-    });
+
+    // Outras funções e eventos do seu código...
+});
