fe1a6cbe2371aef178ec80d4f474821201801776
alexdias777 <74875568+alexdias777@users.noreply.github.com>
2026-02-16 08:50:25 -0300

    update

---
+++ Portifólio/hamburgueria-ecommerce/products.js
@@ -0,0 +1,21 @@
+// Lista de produtos (pode virar pizza, lanche, etc)
+const products = [
+  {
+    id: 1,
+    name: "X-Burger",
+    price: 18.90,
+    image: "img/exemplo 1.jpg"
+  },
+  {
+    id: 2,
+    name: "X-Bacon",
+    price: 22.90,
+    image: "img/exemplo 2.jpg"
+  },
+  {
+    id: 3,
+    name: "X-Tudo",
+    price: 26.90,
+    image: "img/exemplo 3.jpg"
+  }
+];
