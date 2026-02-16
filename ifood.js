f8043bf53be1cdc884ac1e208ea90d1f7abe5d99
alexdias777 <74875568+alexdias777@users.noreply.github.com>
2026-02-16 11:44:44 -0300

    Minha atualização

---
+++ ifood.js
@@ -0,0 +1,9 @@
+
+// Adicionar o evento de clique no botão
+    document.getElementById('GoIfood').addEventListener('click', function() {
+        window.open('https://www.ifood.com.br/delivery/alvorada-rs/bolos-doces-salada-de-frutas---bolo-de-pote-da-ale-passo-do-feijo/a61a583c-b947-4287-a6d0-8230133bbee1', '_blank');
+    }
+// Adicionar o evento de clique no botão
+    document.getElementById('GoIfood').addEventListener('click', function() {
+        window.open('https://www.ifood.com.br/delivery/alvorada-rs/bolos-doces-salada-de-frutas---bolo-de-pote-da-ale-passo-do-feijo/a61a583c-b947-4287-a6d0-8230133bbee1', '_blank');
+    });
